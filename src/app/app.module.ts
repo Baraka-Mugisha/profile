74d67b5872cce9e4e4272921d92676b9eec20485
Baraka <mugishaje@gmail.com>
2021-01-12 16:44:49 +0200

    Routes (#5)
    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LicensesComponent } from './licenses/licenses.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    LicensesComponent,
+    EducationComponent,
+    ContactComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'licenses', component: LicensesComponent },
+      { path: 'education', component: EducationComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
